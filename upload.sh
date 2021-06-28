52e7fa6ef71cba2efa06c97f22b2c03ac4e3bd5f
22ru <22ru@users.noreply.github.com>
2021-06-28 16:34:40 -0700

    dsadsada

---
+++ upload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# another mediocre script by fran hat
+# uploads or deletes only modified files
+# ignores .gitignore and all files ignored as well as the temp file used here
+
+filename='temp'
+
+git status --porcelain > temp
+
+while read line || [[ -n $line ]];
+do
+    action=${line::1}
+    file=${line:3}
+    if [ $file == '.gitignore' ] || [ $file == 'temp' ] || [ $file == 'upload.sh' ]
+    then
+        echo Not touching $file    
+    elif [ $action == 'M' ] || [ $action == 'A' ]
+    then 
+        neocities upload $file
+    elif [ $action == 'D' ]
+    then
+        neocities delete $file
+    fi
+done < temp
