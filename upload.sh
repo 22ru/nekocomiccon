c30763e5a01031b7dddc3e62a8b316020a78ad84
22ru <22ru@users.noreply.github.com>
2021-06-28 17:34:56 -0700

    DSAdas

--- upload.sh
+++ upload.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
 # another mediocre script by fran hat
-# uploads or deletes only modified files since last pushgit 
+# run git add . first
+# uploads or deletes only modified files since last push
 # ignores .gitignore and all files ignored as well as the temp file used here
 
 # must be using git and must have neocities CLI installed
-# place in the base folder! feel free to add it to the git pre-push!
+# place in the base folder! feel free to add it to the git pre-push
 # add the line "./upload.sh" to file .git/hooks/ to do that
 
+# huge issue: neocities uploader only uploads to the root folder.
+# im not sure if its possible to make this work with just the uploader
+
 filename='temp'
+apikey=$(cat $"apikey")
+
+echo $apikey
+
+curl "https://"$apikey"@neocities.org/api/info"
 
 git status --porcelain > temp
 
 while read line || [[ -n $line ]];
 do
     action=${line::1}
-    file=${line:3}
-    if [ $file == '.gitignore' ] || [ $file == 'temp' ] || [ $file == 'upload.sh' ]
+    file=${line:2}
+    if [ $file == '.gitignore' ] || [ $file == 'tempfile' ] || [ $file == 'upload.sh' ] || [ $file == 'apikey' ]
     then
+        #this gets messed up with a rename but doesnt break the code
         echo Not uploading $file    
     elif [ $action == 'M' ] || [ $action == 'A' ]
     then 
-        neocities upload $file
+        echo Uploading $file 
     elif [ $action == 'D' ]
     then
-        neocities delete $file
+        echo Deleting $file 
+    elif [ $action == 'R' ]
+    then
+        echo Renaming $file 
     fi
 done < temp
