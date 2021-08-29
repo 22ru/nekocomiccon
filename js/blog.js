064973869d2dfe63f2b7210af659fe309e36e0f5
22ru <22ru@users.noreply.github.com>
2021-08-28 20:46:04 -0700

    Dsadas

--- js/blog.js
+++ js/blog.js
@@ -12,7 +12,7 @@
 // Will give you a 404 if you enter 0
 // (please someone tell me how to check the number of files 
 // in a directory using vanilla JS)
-var totalPages = 46;
+var totalPages = 47;
 var page = totalPages;
 
 // Run on load of main body in document
