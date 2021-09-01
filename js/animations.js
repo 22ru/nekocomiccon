63d96545a0c261289d24096c62ec649aaed4d3e6
22ru <22ru@users.noreply.github.com>
2021-08-31 23:15:03 -0700

    Dsadas

--- js/animations.js
+++ js/animations.js
@@ -3,14 +3,18 @@ var y = 0;
 var i = 0;
 
 var bg;
+var ti;
+
 
 function startAnimations() {
     document.getElementById("animationButton").setAttribute("onClick","endAnimations()");
     bg = setInterval(moveBackground, 70);
+    ti = setInterval(vibrate, 100);
 }
 function endAnimations() {
     document.getElementById("animationButton").setAttribute("onClick","startAnimations()");
     clearInterval(bg);
+    clearInterval(ti);
 }
 
 function moveBackground() {
@@ -20,4 +24,42 @@ function moveBackground() {
 
     document.getElementById("main").style.backgroundPositionX = x + "px";
     document.getElementById("main").style.backgroundPositionY = y + "px";
+}
+
+function vibrate() {
+    var t = document.getElementsByTagName("h1")[0];
+    var rand = Math.floor(Math.random()*15);
+    if (rand == 0) {
+        t.innerText = "ハットのnekoコミコン";
+    } else if (rand == 1) {
+        t.style.fontFamily = "Arial";
+    } else if (rand == 2) {
+        t.innerText = "HAT の nekocomicコン";        
+    } else if (rand == 3) {
+        t.style.color = "#ed87a6";
+    } else if (rand == 4) {
+        t.innerText = "hat's nekoコミコン";
+    } else if (rand == 5) {
+        t.style.color = "#d2272f";
+    } else if (rand == 6) {
+        t.innerText = "ハット's 猫コミコン";
+    } else if (rand == 7) {
+        t.innerText = "hat's 猫comiccon";
+    } else if (rand == 8) {
+        t.style.color = "#e87507";
+    } else if (rand == 9) {
+        t.style.color = "#fccf4f";
+    } else if (rand == 10) {
+        t.style.fontFamily = "Monaco";
+    } else if (rand == 11) {
+        t.style.color = "#f7cc77";
+    } else if (rand == 12) {
+        t.style.fontFamily = "Times New Roman";
+    } else if (rand == 13) {
+        t.style.fontFamily = "Copperplate";
+    } else if (rand == 14) {
+        t.style.fontFamily = "Courier New";
+    }
+    t.style.marginTop = 21 + rand/2 + "px";
+    t.style.marginLeft = rand*2 + "px";
 }
---
+++ js/ratingsystem.js
@@ -0,0 +1,31 @@
+rating = Math.floor((Date.now()-1630476455735) / 300000);
+
+function refreshScore() {
+    document.getElementById("currentRating").innerText = "Current rating: " + rating;
+}
+
+function upvote() {
+    rating++;
+    document.getElementById("response").innerText = "thank you!!!!";
+    refreshScore();
+}
+
+function downvote() {
+    rating--;
+    document.getElementById("response").innerText = "thank you for your feedback.";
+    refreshScore();
+}
+
+function changeRating() {
+    if (Math.round(Math.random(1)) == 1) {
+        rating++;
+    } else {
+        rating--;
+    }
+    refreshScore();
+}
+
+function startRating() {
+    window.setInterval(changeRating, 4853);
+    refreshScore();
+}
