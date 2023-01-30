f153e3ecde091184ef106e2da7911714b94ddd0e
22ru <22ru@users.noreply.github.com>
2023-01-30 13:16:18 -0800

    dsadsadas

--- cafe/img/memoryconvert.sh
+++ cafe/img/memoryconvert.sh
@@ -4,8 +4,8 @@ echo "Converting folder $2 in $1!";
 
 cd $1/$2;
 
-magick mogrify -resize 500x500 -format jpg *.jpg;
-rm *.png;
+magick mogrify -resize 500x500 -format jpg *.JPG;
+rm *.JPG;
 echo "Memories shrank!";
 
 touch memories;
@@ -14,7 +14,7 @@ echo "$1" > memories;
 
 for f in *.jpg ; do 
 	echo '<div class="memory">' >> memories;
-	echo -e "\t<img src=\"../img/$1/$2$f\" />" >> memories; 
+	echo -e "\t<img src=\"../../img/$1/$2$f\" />" >> memories; 
 	echo '</div>' >> memories;
 done;
 
