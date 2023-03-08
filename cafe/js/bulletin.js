f82e39a3daf2ce926b1c0dd5d15be26cf5715833
22ru <22ru@users.noreply.github.com>
2023-03-07 23:30:49 -0800

    dsadasdsada

--- cafe/js/bulletin.js
+++ cafe/js/bulletin.js
@@ -1,11 +1,7 @@
 // To do:
-// randomize offset in placement and rotation
-// look into flex rotation issue?
 // textures for washi tape and photo backing
 // inner photo measurements don't look correct. find a better source for measurements.
 //corkboard could tile a lot better
-
-
 
 class photograph {
     constructor(imgLink, format, physical, rotate, text) {
@@ -17,37 +13,29 @@ class photograph {
     }
 }
 
-var longString = "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little 'clever' comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.";
-
-var photoArray = [
-    new photograph("img/gallery/hina/HAT00194.jpg", "instaxmini", 0,  0, longString),
-    new photograph("img/gallery/hina/HAT00194.jpg", "instaxsquare", 0,  0, longString),
-    new photograph("img/gallery/hina/HAT00194.jpg", "instaxwide", 0,  0, longString),
-
-    new photograph("img/bulletin/IMG_9561.jpg", "instaxmini", 0,  0, "Cursed Hybrid<br />11/17/2022"),
-    new photograph("img/bulletin/IMG_9561.jpg", "instaxmini", 0, 1, "Cursed Hybrid<br />11/17/2022"),
-    new photograph("img/bulletin/IMG_9717.jpg", "instaxmini", 1, 0, "cool text"),
-    new photograph("img/bulletin/IMG_9717.jpg", "instaxmini", 1, 1, "hi")
-]
-
-
 function loadPhotos() {
-    var i, photo;
+    var i, photo, img, pin, offset;
     for (i = 0; i < photoArray.length; i++) {
 
         photo = document.createElement("div");
         photo.className = photoArray[i].format;
 
+        offset = Math.random()*200 - 100;
+
         if (photoArray[i].physical == 1) {
             photo.style["backgroundImage"] = "url(" + photoArray[i].imgLink + ")";
             photo.style["backgroundSize"] = "cover";
-            
         } else {
             img = document.createElement("img");
             img.src = photoArray[i].imgLink;
-
+            img.className = "photo";
             photo.appendChild(img);
         }
+
+        pin = document.createElement("img");
+        pin.className = "pin";
+        pin.src = "img/assets/violet.gif";
+        photo.appendChild(pin);
 
         if (photoArray[i].text.length > 0) {
             writing = document.createElement("p");
@@ -55,15 +43,33 @@ function loadPhotos() {
             if (photoArray[i].physical == 1) {
                 writing.className = "overlayTape";
             }
+            writing.style["rotate"] = offset / 40 + "deg";
             photo.appendChild(writing);
         }
 
-        if (photoArray[i].rotate == 1) {
-            photo.style["rotate"] = "-91deg";
-            photo.style["filter"] = "drop-shadow(-5px 5px 5px rgb(0,0,0,.4))";
-        }
-
         document.getElementById("board").appendChild(photo);
-    }
 
+        if (photoArray[i].rotate == 1) {
+            pin.style["rotate"] = 90 + offset % 10 + "deg";
+            photo.style["rotate"] = -90 + offset % 10 + "deg";
+            photo.style["filter"] = "drop-shadow(-5px 5px 5px rgb(0,0,0,.4))";
+            pin.style["top"] = "50%";
+            pin.style["left"] = "revert";
+            pin.style["right"] = "2mm";
+            if (photoArray[i].format == "instaxwide") {
+                margin = (parseInt(getComputedStyle(photo).getPropertyValue("width"), 10) - parseInt(getComputedStyle(photo).getPropertyValue("height"), 10)) / 2;
+                photo.style["marginTop"] = margin + "px";
+                photo.style["marginBottom"] = margin + "px";
+            } else {
+                margin = (parseInt(getComputedStyle(photo).getPropertyValue("height"), 10) - parseInt(getComputedStyle(photo).getPropertyValue("width"), 10)) / 2;
+                photo.style["marginLeft"] = margin + "px";
+                photo.style["marginRight"] = margin + "px";
+            }
+        } else {
+            photo.style["rotate"] = offset % 10 + "deg";
+            photo.style["marginTop"] = offset % 10 + "mm";
+            photo.style["marginLeft"] = offset / 10 + "mm";
+            pin.style["rotate"] = offset % 10 + "deg";
+        }
+    }
 }
---
+++ cafe/js/bulletindata.js
@@ -0,0 +1,29 @@
+
+// Many formats good for testing!
+/*var photoArray = [
+    new photograph("img/gallery/hina/HAT00194.jpg", "instaxmini", 0,  0, longString),
+    new photograph("img/gallery/hina/HAT00194.jpg", "instaxsquare", 0,  0, longString),
+    new photograph("img/gallery/hina/HAT00194.jpg", "instaxwide", 0,  0, longString),
+
+    new photograph("img/bulletin/IMG_9561.jpg", "instaxmini", 0,  0, "Cursed Hybrid<br />11/17/2022"),
+    new photograph("img/bulletin/IMG_9561.jpg", "instaxmini", 0, 1, "Cursed Hybrid<br />11/17/2022"),
+    new photograph("img/bulletin/IMG_9717.jpg", "instaxmini", 1, 0, "cool text"),
+    new photograph("img/bulletin/IMG_9717.jpg", "instaxmini", 1, 1, "hi"),
+    new photograph("img/gallery/hina/HAT00194.jpg", "instaxsquare", 0,  1, longString),
+    new photograph("img/gallery/hina/HAT00194.jpg", "instaxwide", 0,  1, longString),
+]*/
+
+var longString = "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little 'clever' comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.";
+
+var photoArray = [
+    new photograph("img/bulletin/01-15-2023.jpg", "instaxmini", 1, 0, ""),
+    new photograph("img/bulletin/11-24-2022.jpg", "instaxmini", 1, 1, ""),
+    new photograph("img/bulletin/11-20-2022.jpg", "instaxmini", 1, 0, ""),
+    new photograph("img/bulletin/IMG_9561.jpg", "instaxmini", 0, 0, "Cursed Hybrid<br />11/17/2022"),
+    new photograph("img/bulletin/10-13-2022.jpg", "instaxmini", 1, 1, ""),
+    new photograph("img/bulletin/09-17-2022.jpg", "instaxmini", 1, 1, ""),
+    new photograph("img/bulletin/08-22-2022b.jpg", "instaxmini", 1, 0, ""),
+    new photograph("img/gallery/hina/HAT00194.jpg", "instaxsquare", 0,  0, longString),
+    new photograph("img/bulletin/08-22-2022a.jpg", "instaxmini", 1, 0, ""),
+    new photograph("img/bulletin/07-31-2022.jpg", "instaxmini", 1, 1, ""),
+]
